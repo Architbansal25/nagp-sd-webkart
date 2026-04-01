17045dec257bc9c9d0e72f61879bda2c201c75de
architbansal <archit.bansal@@nagarro.com>
2026-04-01 12:41:10 +0530

    updated

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "a9dc6e0314fa842deb45b8428daa5fcc-173269365.ap-south-1.elb.amazonaws.com";
+export const BACKEND_URL = "a4ae5e0a381904769aefe86c4dde5e45-104992364.ap-south-1.elb.amazonaws.com";
