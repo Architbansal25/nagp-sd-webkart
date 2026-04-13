6fefad02d5b7c0c0dc18e9caaacda06e021e1dba
Architbansal25 <63822989+Architbansal25@users.noreply.github.com>
2026-04-14 00:37:40 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,10 +13,9 @@ const Home = () => {
       <Carousel></Carousel>
       {/* Static Banner */}
       <div className="p-8 "> <Banner></Banner></div>
-  
+      <h1>hello change </h1>
       {/* "Shop By Category" Section */}
       <div className="text-center mt-14">
-      <h1>hello change</h1>
         <Categories></Categories>
       </div>
     </div>
